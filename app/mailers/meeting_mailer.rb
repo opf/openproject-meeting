3f9ffe254dadc062bfbcb0d544eaa9f1bb87cbb9
Fabrizio Bertone <bertone@ismb.it>
2016-01-27 17:27:41 +0100

    Add iCalendar support

--- app/mailers/meeting_mailer.rb
+++ app/mailers/meeting_mailer.rb
@@ -29,4 +29,43 @@ class MeetingMailer < UserMailer
     subject = "[#{@meeting.project.name}] #{I18n.t(:"label_#{content_type}")}: #{@meeting.title}"
     mail to: address, subject: subject
   end
+  
+  def publish_icalendar(content, content_type, address)
+    @meeting = content.meeting
+    @content_type = content_type
+    
+    open_project_headers 'Project' => @meeting.project.identifier,
+                         'Meeting-Id' => @meeting.id
+    headers['Content-Type'] = 'text/calendar; charset=utf-8; method="PUBLISH"; name="meeting.ics"'
+    headers['Content-Transfer-Encoding'] = '8bit'
+    subject = "[#{@meeting.project.name}] #{I18n.t(:"label_#{content_type}")}: #{@meeting.title}"
+    mystartdate = @meeting.start_time.strftime("%Y%m%dT%H%M%SZ")
+    myenddate = @meeting.end_time.strftime("%Y%m%dT%H%M%SZ")
+    url = meeting_url(@meeting)
+    now = DateTime.now.strftime("%Y%m%dT%H%M%SZ")
+
+    ical = "BEGIN:VCALENDAR
+VERSION:2.0
+PRODID:OpenProject Meeting
+BEGIN:VEVENT
+SUMMARY:[#{@meeting.project.name}] #{@meeting.title}
+LOCATION:#{@meeting.location}
+DTSTAMP:#{now}
+DTSTART:#{mystartdate}
+DTEND:#{myenddate}
+DESCRIPTION:
+METHOD:PUBLISH
+ORGANIZER;CN=\"#{@meeting.author}\":MAILTO:#{@meeting.author.mail}
+UID:#{@meeting.id}@#{@meeting.project.identifier}
+SEQUENCE:0
+URL:#{url}
+END:VEVENT
+END:VCALENDAR"
+
+    mail(to: address, subject: subject) do |format|
+       format.ics {
+         render :text => ical, :layout => false
+      }
+    end
+  end
 end
